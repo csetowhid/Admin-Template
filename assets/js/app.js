d00c4548637fdb23bdf69fc953072a324ce6f815
Towhid Hasan <towhidhasang1@gmail.com>
2021-09-23 20:09:08 +0600

    Sidebar Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,11 @@
 $(document).on('click', '#sidebar li', function() {
     $(this).addClass('active').siblings().removeClass('active')
 });
+
+// ===============left menu sidebar dp toggle========
+
+$('.sub-menu ul').hide();
+$(".sub-menu a").click(function() {
+    $(this).parent(".sub-menu").children("ul").slideToggle("100");
+    $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+});
